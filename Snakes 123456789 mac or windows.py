acefc793951a09b8d1c32f3ef9b4162cb30009d6
VipEr-sta <elijah.goglin@gmail.com>
2020-12-24 22:42:30 -0500

    Clean up and starting to work on more keybinds

--- Snakes 123456789 mac or windows.py
+++ Snakes 123456789 mac or windows.py
@@ -1,22 +1,17 @@
 import turtle
-
 import time
-
 import random
+from pynput.keyboard import Key, Controller
 
-
+keyboard = Controller()
 
 delay = 0.1
-
-
 
 # Score
 
 score = 0
 
 high_score = 0
-
-
 
 # Set up the screen
 
@@ -28,9 +23,7 @@ wn.bgcolor("green")
 
 wn.setup(width=600, height=600)
 
-wn.tracer(0) # Turns off the screen updates
-
-
+wn.tracer(0)  # Turns off the screen updates
 
 # Snake Head
 
@@ -44,11 +37,9 @@ head.color("black")
 
 head.penup()
 
-head.goto(0,0)
+head.goto(0, 0)
 
 head.direction = "stop"
-
-
 
 # Snake Food
 
@@ -62,9 +53,7 @@ food.color("red")
 
 food.penup()
 
-food.goto(0,100)
-
-
+food.goto(0, 100)
 
 segments = []
 
@@ -90,77 +79,62 @@ pen.write("Score: 0 High Score: 0", align="center", font=("Courier", 24, "normal
 # Code must be refactored here
 # Function
 
+
 def go_up():
-
     if head.direction != "down":
-
         head.direction = "up"
 
 
-
 def go_down():
-
     if head.direction != "up":
-
         head.direction = "down"
 
 
-
 def go_left():
-
     if head.direction != "right":
-
         head.direction = "left"
 
 
-
 def go_right():
-
     if head.direction != "left":
-
         head.direction = "right"
 
 
-
 def move():
-
     if head.direction == "up":
-
         y = head.ycor()
 
         head.sety(y + 20)
 
-
-
-    if head.direction == "down":
-
+    elif head.direction == "down":
         y = head.ycor()
 
         head.sety(y - 20)
 
-
-
-    if head.direction == "left":
-
+    elif head.direction == "left":
         x = head.xcor()
 
         head.setx(x + 20)
 
-
-
-    if head.direction == "right":
-
+    elif head.direction == "right":
         x = head.xcor()
 
         head.setx(x - 20)
+    else:
+        return
+
+def die():
+    pass
 
 
 
 # Keyboard bindings
 
+#WASD
+
 wn.listen()
 
-wn.onkeypress(go_up, "w")
+wn.onkeypress(go_up, "w" or "")
 
 wn.onkeypress(go_down, "s")
 
@@ -169,58 +143,42 @@ wn.onkeypress(go_left, "d")
 wn.onkeypress(go_right, "a")
 
 
-
 # Main game loop
 
 while True:
 
     wn.update()
 
-
-
     # Check for a collision with the border
 
-    if head.xcor()>290 or head.xcor()<-290 or head.ycor()>290 or head.ycor()<-290:
+    if head.xcor() > 290 or head.xcor() < -290 or head.ycor() > 290 or head.ycor() < -290:
 
         time.sleep(1)
 
-        head.goto(0,0)
+        head.goto(0, 0)
 
         head.direction = "stop"
-
-
 
         # Hide the segments
 
         for segment in segments:
-
             segment.goto(1000, 1000)
-
-
 
         # Clear the segments list
 
         segments.clear()
 
-
-
         # Reset the score
 
         score = 0
-
-
 
         # Reset the delay
 
         delay = 0.1
 
-
-
         pen.clear()
 
         pen.write("Score: {}  High Score: {}".format(score, high_score), align="center", font=("Courier", 24, "normal"))
-
-
 
     # Check for a collision with the food
 
@@ -232,9 +190,7 @@ while True:
 
         y = random.randint(-290, 290)
 
-        food.goto(x,y)
-
-
+        food.goto(x, y)
 
         # Add a segment
 
@@ -250,59 +206,40 @@ while True:
 
         segments.append(new_segment)
 
-
-
         # Shorten delay
 
         delay -= 0.001
-
-
 
         # Increase Score
 
         score += 10
 
-
-
         if score > high_score:
-
             high_score = score
-
-
 
         pen.clear()
 
         pen.write("Score: {}  High Score: {}".format(score, high_score), align="center", font=("Courier", 24, "normal"))
 
-
-
     # Move the end segments first in reverse order
 
-    for index in range(len(segments)-1, 0, -1):
+    for index in range(len(segments) - 1, 0, -1):
+        x = segments[index - 1].xcor()
 
-        x = segments[index-1].xcor()
+        y = segments[index - 1].ycor()
 
-        y = segments[index-1].ycor()
-
-        segments[index].goto(x,y)
-
-
+        segments[index].goto(x, y)
 
     # Move segment 0 to where the head is
 
     if len(segments) > 0:
-
         x = head.xcor()
 
         y = head.ycor()
 
-        segments[0].goto(x,y)
-
-
+        segments[0].goto(x, y)
 
     move()
-
-
 
     # Check for head collision with the body segments
 
@@ -312,56 +249,34 @@ while True:
 
             time.sleep(1)
 
-            head.goto(0,0)
+            head.goto(0, 0)
 
             head.direction = "stop"
-
-
 
             # Hide the segments
 
             for segment in segments:
-
                 segment.goto(1000, 1000)
-
-
 
             # Clear the segments list
 
             segments.clear()
 
-
-
             # Reset the score
 
             score = 0
-
-
 
             # Reset the delay
 
             delay = 0.1
 
-
-
             # Update the score display
 
             pen.clear()
 
-            pen.write("Score: {}  High Score: {}".format(score, high_score), align="center", font=("Courier", 24, "normal"))
-
-
-
-
+            pen.write("Score: {}  High Score: {}".format(score, high_score), align="center",
+                      font=("Courier", 24, "normal"))
 
     time.sleep(delay)
 
-
-
-
-
-
-
-wn, turtle.mainloop()
-
-
+    wn, turtle.mainloop()
